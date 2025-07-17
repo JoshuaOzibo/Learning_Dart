6d82c3fe8268dbc79806fb7dccc489e45e4e792a
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-07-17 12:58:33 +0100

    tried testing my ability

---
+++ practice/test.dart
@@ -0,0 +1,76 @@
+
+// void main(){
+    
+//     // for(int i = 1; i <= 10; i++){
+//     // print(i);
+    
+//     // };
+
+//     int num = 1;
+
+//     while(num <= 20){
+//         if(num % 2 == 0){
+//             print(num); // prints even numbers from 1 to 20
+//         };
+
+//         num++; // increment num by 1
+//     };
+
+//     // number 3 for loops 
+//     // List<String> fruits = ['Apple', 'Mango', 'Banana'];
+//     // for(var fruit in fruits){
+//     //     print(fruit);
+//     // };
+
+//     int i = 1;
+//     do {
+//         print("Learning Dart $i");
+//         i++; // increment i by 1
+//     } while (i <= 5); // loop until i is less than or equal to 5
+
+// };
+
+
+// Test two
+// void main(){
+//     // task one
+//     var favorite_colors = ["balck", "yellow", "white", "blue", "brown"];
+//     // for(var color in favorite_colors){
+//     //     print(color);
+//     // };
+
+
+//     // task two
+//     var newColors = ["purple", "pink", "arch"];
+//     favorite_colors.addAll(newColors);
+
+//     // task three;
+//     favorite_colors.removeAt(0);
+
+//     // task four
+    
+//         if(favorite_colors.contains("red")){
+//             print("red is in the list");
+//         }else{
+//             print("red is not in the list");
+//         };
+
+//     print(favorite_colors);
+// }
+
+
+// test three
+void main() {
+    // task one
+    var student = {"name": "joshua", "age": "10", "grade": "jss2"};
+
+    // task two
+    print(student["name"]);
+
+    // task three;
+    student["school"] = "ABC High School";
+
+    // task four
+    print(student);
+
+}

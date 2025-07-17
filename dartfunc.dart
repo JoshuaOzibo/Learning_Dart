bd0f8a7ef088075941af9c65aa74b0a3d0eccf4a
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-07-17 13:26:13 +0100

    functions with dart

---
+++ dartfunc.dart
@@ -0,0 +1,18 @@
+void main(){
+    // functions in dart
+    // myFunc(int num){
+    //     print(num + num);
+    // }
+
+    // myFunc(5);
+
+    // newFunc(String num1, {num2}){
+
+    // };
+
+    // var names = newFunc("jOSHUA", num2: "samuel");
+
+    // print(names);
+
+  
+}

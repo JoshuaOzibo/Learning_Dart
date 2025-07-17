4348eb5b1202f6e9daf331ff35f188aa65e5edfe
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-07-17 13:08:01 +0100

    logic with dart

---
+++ dartlogic.dart
@@ -0,0 +1,10 @@
+void main(){
+    // Logic - if , if/Else, if Else if
+
+    var num = 5;
+    if(num == 5){
+        print("number is $num");
+    }else if(num != 5){
+        print("number $num is not 5");
+    }
+}

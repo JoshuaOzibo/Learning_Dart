45ad537d6a48ba8f41b1a176ad9a39f9ebc5388a
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-08-01 12:56:26 +0100

    refactor: reorganize main function and improve code readability

--- dartmaps.dart
+++ dartmaps.dart
@@ -1,29 +1,62 @@
-void main(){
-    // Maps Key/Value Pairs
-    var toppings = {"John": "Pepperoni", "Mary": "Cheese"}; //note john and mary are keys while pepperoni and cheese are values;
-    // print(toppings["John"]);
 
-    // Show Values:
-    // print(toppings.values);
-
-    // Show Keys
-    // print(toppings.keys);
-
-    // Show Length
-    // print(toppings.length);
-
-    // Add Something
-    // toppings["Tim"] = "Sausage";
-
-    // Add Many things
-    toppings.addAll({"joshua":"ozibo", "michael": "chinedu"});
-
-    // Remove Something
-    toppings.remove("michael"); // Note you can only remove key pairs
-
-    // Remove Everything
-    toppings.clear();
+// import http/http.dart as http;
+// import 'package:http/http.dart' as http;
 
 
-    print(toppings);
+void main() async {
+  // Maps Key/Value Pairs
+  var toppings = {
+    "John": "Pepperoni",
+    "Mary": "Cheese",
+  }; //note john and mary are keys while pepperoni and cheese are values;
+  // print(toppings["John"]);
+
+  // Show Values:
+  // print(toppings.values);
+
+  // Show Keys
+  // print(toppings.keys);
+
+  // Show Length
+  // print(toppings.length);
+
+  // Add Something
+  // toppings["Tim"] = "Sausage";
+
+  // Add Many things
+  toppings.addAll({"joshua": "ozibo", "michael": "chinedu"});
+
+  // Remove Something
+  toppings.remove("michael"); // Note you can only remove key pairs
+
+  // Remove Everything
+  toppings.clear();
+  print(toppings);
+
+  List<Map<String, String>> marks = [ // list of map more like array of object in javascript
+    {"josh": "10"},
+
+    {"mark": "20"},
+
+    {"sam": "30"},
+  ];
+
+  marks.map((e){
+    e.forEach((key, value){
+      print("$key, $value");
+    });
+  }).toList();
+
+  // print(marks["sonner"] == null ? "this key dosent exist" : "is there");
+
+
+// final result = await getResult();
+
+// print(result);
+  
 }
+
+
+// Future<List<Map>> getResult() async {
+//     return Future.delayed(Duration(seconds: 2));
+//   }
